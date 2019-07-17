c4515daa7edca8ce4332471e6702b6dc8f98cf4c
Francesco Prospato <fprospato@gmail.com>
2019-07-17 11:46:38 -0700

    - Add code for Home Activity that was not pushing due to merge conflicts - Add UI improvements to Home Activity

--- app/src/main/java/com/hkim00/moves/HomeActivity.java
+++ app/src/main/java/com/hkim00/moves/HomeActivity.java
@@ -1,10 +1,14 @@
 package com.hkim00.moves;
 
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.net.PlacesClient;
@@ -20,6 +24,7 @@ import cz.msebera.android.httpclient.Header;
 public class HomeActivity extends AppCompatActivity {
 
     public static final String API_BASE_URL = "https://maps.googleapis.com/maps/api/place";
+    private static int screenWidth;
 
     AsyncHttpClient client;
 
@@ -34,6 +39,14 @@ public class HomeActivity extends AppCompatActivity {
     Button btnPlace;
     Button btnPrice;
 
+    ConstraintLayout clCategories;
+    ImageView ivFood;
+    ImageView ivActivities;
+    ImageView ivAttractions;
+    ImageView ivEvents;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,16 +56,52 @@ public class HomeActivity extends AppCompatActivity {
         Places.initialize(getApplicationContext(), getString(R.string.api_key));
         PlacesClient placesClient = Places.createClient(this);
 
+        client = new AsyncHttpClient();
+
+        getViewIds();
+
+        setupDesign();
+
+        getNearbyPlaces();
+    }
+
+    private void getViewIds(){
         tvLocation = findViewById(R.id.tvLocation);
         btnTime = findViewById(R.id.btnTime);
         btnPeople = findViewById(R.id.btnPeople);
         btnPlace = findViewById(R.id.btnPlace);
         btnPrice = findViewById(R.id.btnPrice);
-
-        client = new AsyncHttpClient();
-
-        getNearbyPlaces();
+        clCategories = findViewById(R.id.clCategories);
+        ivFood = findViewById(R.id.ivFood);
+        ivActivities = findViewById(R.id.ivActivities);
+        ivAttractions = findViewById(R.id.ivAttractions);
+        ivEvents = findViewById(R.id.ivEvents);
     }
+
+
+    private void setupDesign() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        screenWidth = size.x;
+
+        btnTime.getLayoutParams().width= screenWidth/4;
+        btnPeople.getLayoutParams().width= screenWidth/4;
+        btnPlace.getLayoutParams().width= screenWidth/4;
+        btnPrice.getLayoutParams().width= screenWidth/4;
+
+        clCategories.post(new Runnable() {
+            @Override
+            public void run() {
+                int constraintHeight = clCategories.getLayoutParams().height;
+                ivFood.getLayoutParams().height = constraintHeight/4;
+                ivActivities.getLayoutParams().height = constraintHeight/4;
+                ivAttractions.getLayoutParams().height = constraintHeight/4;
+                ivEvents.getLayoutParams().height = constraintHeight/4;
+            }
+        });
+    }
+
 
     private void getNearbyPlaces() {
         String apiUrl = API_BASE_URL + "/nearbysearch/json";
