b8cf8438bd020dbb67bba706ad0f27464be957b4
hma17 <huda.aldadah@duke.edu>
2019-07-17 13:20:22 -0700

    profile activity

--- app/src/main/java/com/hkim00/moves/models/ProfileActivity.java
+++ app/src/main/java/com/hkim00/moves/models/ProfileActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.hkim00.moves.R;
+
 public class ProfileActivity extends AppCompatActivity {
 
     @Override
