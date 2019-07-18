e0ad80710a861427e882c8feaeb335ae6a7d5706
Francesco <fprospato@gmail.com>
2019-07-18 14:23:14 -0700

    - Move entire Profile Activity to a Profile Fragment - Go to Profile Fragment from navbar

--- app/src/main/java/com/hkim00/moves/HomeActivity.java
+++ app/src/main/java/com/hkim00/moves/HomeActivity.java
@@ -1,31 +1,13 @@
 package com.hkim00.moves;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Point;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.Display;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -33,20 +15,8 @@ import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.hkim00.moves.fragments.HomeFragment;
-import com.hkim00.moves.models.Restaurant;
+import com.hkim00.moves.fragments.ProfileFragment;
 import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.JsonHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.parceler.Parcels;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import cz.msebera.android.httpclient.Header;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -92,7 +62,7 @@ public class HomeActivity extends AppCompatActivity {
                         fragment = new HomeFragment();
                         break;
                     case R.id.action_profile:
-                        fragment = new HomeFragment();
+                        fragment = new ProfileFragment();
                         Toast.makeText(HomeActivity.this, "profile", Toast.LENGTH_SHORT).show();
                     default:
                         break;
