ff86fdf79cf55c7fa3abc1700446d25f937f2d2f
hma17 <huda.aldadah@duke.edu>
2019-07-18 12:01:45 -0700

    Move Details Activity

---
+++ app/src/main/java/com/hkim00/moves/MoveDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.hkim00.moves;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoveDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_details);
+    }
+}
