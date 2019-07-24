17d901b9cdd987d2f49cfcfb6ab1b96d6de1735d
Francesco <fprospato@gmail.com>
2019-07-24 15:49:34 -0700

    - Add spec folder with MoveItemSpec - Add Litho to gradle - Code clean up for LoginActivity and ProfileFragment

---
+++ app/src/main/java/com/hkim00/moves/specs/MoveItemSpec.java
@@ -0,0 +1,29 @@
+package com.hkim00.moves.specs;
+
+import android.graphics.Color;
+
+import com.facebook.litho.Column;
+import com.facebook.litho.Component;
+import com.facebook.litho.ComponentContext;
+import com.facebook.litho.annotations.LayoutSpec;
+import com.facebook.litho.annotations.OnCreateLayout;
+import com.facebook.litho.widget.Text;
+import com.facebook.yoga.YogaEdge;
+
+@LayoutSpec
+public class MoveItemSpec {
+
+    @OnCreateLayout
+    static Component onCreateLayout(ComponentContext c) {
+
+        return Column.create(c)
+                .paddingDip(YogaEdge.ALL, 15)
+                .backgroundColor(Color.WHITE)
+                .child(
+                        Text.create(c)
+                                .text("test")
+                                .textSizeDip(20))
+                .build();
+    }
+
+}
