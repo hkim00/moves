e3cfef0c106b95e29c28e0f6ac5924f062d5e806
Francesco <fprospato@gmail.com>
2019-07-25 17:42:50 -0700

    Add slide between home fragments

--- app/src/main/java/com/hkim00/moves/HomeActivity.java
+++ app/src/main/java/com/hkim00/moves/HomeActivity.java
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.hkim00.moves.fragments.HistoryFragment;
@@ -27,6 +28,7 @@ public class HomeActivity extends AppCompatActivity {
     FragmentManager fragmentManager;
     private BottomNavigationView bottomNavigation;
 
+    private int currentFrag = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,22 +49,38 @@ public class HomeActivity extends AppCompatActivity {
         bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                FragmentTransaction fts = getSupportFragmentManager().beginTransaction();
                 Fragment fragment = new HomeFragment();
+
 
                 switch (menuItem.getItemId()) {
                     case R.id.action_history:
                         fragment = new HistoryFragment();
+                        fts.setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_right);
+                        currentFrag = 0;
                         break;
                     case R.id.action_home:
                         fragment = new HomeFragment();
+
+                        if (currentFrag < 1) {
+                            fts.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left);
+                        } else {
+                            fts.setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_right);
+                        }
+
+                        currentFrag = 1;
                         break;
                     case R.id.action_profile:
                         fragment = new ProfileFragment();
+                        fts.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left);
+                        currentFrag = 2;
                     default:
                         break;
                 }
 
-                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                fts.replace(R.id.flContainer, fragment, "fragment");
+                fts.commit();
+
                 return true;
             }
         });
